ddf512dfca0fdb115af7f939b6beb26cf2161b45
samadii <77415087+samadii@users.noreply.github.com>
2021-06-02 01:30:34 +0430

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 class Config(object):
 
-TOKEN = "1821784179:AAHQWfHb9X5s-F6QM5A3FbwRHOgl4DgVkFM"
+TOKEN = os.environ.get("TOKEN", "")
 USERNAME = os.environ.get("USERNAME", "")
 BOT_CHAT_ID = int(os.environ.get("CHAT", ""))
