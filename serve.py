13320a381dd353eafb8c68b5450307a0855ba84e
samadii <77415087+samadii@users.noreply.github.com>
2021-06-02 02:55:45 +0430

    Update serve.py

--- serve.py
+++ serve.py
@@ -3,7 +3,6 @@
 # Author: Racter Liu (Racterub / root@racterub.me)
 
 from telegram.ext import Updater, Filters, MessageHandler
-from config import TOKEN, BOT_CHAT_ID, USERNAME
 
 
 def file_handler(update, context):
@@ -19,7 +18,7 @@ def file_handler(update, context):
         fileName = update.message['document']['file_name']
         caption = update.message['caption']
         context.bot.sendDocument(
-            chat_id = BOT_CHAT_ID,
+            chat_id = -1001264182630,
             filename = fileName,
             caption = caption,
             document = fileID
@@ -29,33 +28,33 @@ def file_handler(update, context):
         fileID = update.message['photo'][-1]['file_id']
         caption = update.message['caption']
         context.bot.sendPhoto(
-        chat_id = BOT_CHAT_ID,
+        chat_id = -1001264182630,
         caption = caption,
         photo = fileID
     )
 
 def message_handler(update, context):
     context.bot.sendMessage(
-        chat_id=BOT_CHAT_ID,
+        chat_id=-1001264182630,
         text=f"{update.message.text}"
     )
 
 
 if __name__=='__main__':
-    updater = Updater(token=TOKEN, use_context=True)
+    updater = Updater(token="1821784179:AAHQWfHb9X5s-F6QM5A3FbwRHOgl4DgVkFM", use_context=True)
     dispatcher = updater.dispatcher
 
     #Add Image/File handler
     dispatcher.add_handler(
         MessageHandler(
-            (Filters.document | Filters.photo) & Filters.user(username=f"@{USERNAME}"),
+            (Filters.document | Filters.photo) & Filters.user(username=f"@Pgffhjsejahjj"),
         file_handler
         )
     )
     #Add Message handler
     dispatcher.add_handler(
         MessageHandler(
-            (Filters.text | (~Filters.command)) & Filters.user(username=f"@{USERNAME}"),
+            (Filters.text | (~Filters.command)) & Filters.user(username=f"@Pgffhjsejahjj"),
             message_handler
         )
     )
