6d164a0caf607ef469b8edcaf9af45f9e59178f5
samadii <77415087+samadii@users.noreply.github.com>
2021-06-02 04:11:53 +0430

    Update serve.py

--- serve.py
+++ serve.py
@@ -17,11 +17,12 @@ def file_handler(update, context):
         fileID = update.message['audio']['file_id']
         fileName = update.message['audio']['file_name']
         performer = update.message['audio']['performer']
+        title = update.message['audio']['title']
         caption = update.message['caption']
         context.bot.sendAudio(
             chat_id = -1001264182630,
             filename = fileName,
-            caption = performer,
+            caption = performer + title,
             audio = fileID
         )
     else:
