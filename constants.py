e6186701322a611fc876e4993d654939c8423221
samadii <77415087+samadii@users.noreply.github.com>
2021-07-01 03:08:00 +0430

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,1 +1,1 @@
-welcome_text = 'Hi!\nWelcome to Optical Character Recognizer Bot.\nJust send a clear image to me and I will recognize the text in the image and send it as a message!'
+welcome_text = 'Hi !\nI am Music Caption Adder Bot !\n\nI add a dynamic caption to the musics,\nFirst add me to a channel as admin, then send me the musics, i will post them in the channel.'
